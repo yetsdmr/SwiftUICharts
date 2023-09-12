787f3a51eed64a0b83b5d9d07f49096d1f8ac837
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-12 15:34:52 +0300

    Initial Commit

---
+++ SwiftUICharts/SwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsApp.swift
+//  SwiftUICharts
+//
+//  Created by Yunus Emre Taşdemir on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
